5086f63c0684e798246a29a30a700e1226012a0e
taj106 <sirtaj.pandher1@marist.edu>
2024-09-06 12:22:41 -0400

    hw1

--- Homework 1/Debug.java
+++ Homework 1/Debug.java
@@ -1,11 +1,10 @@
 //always start with importing our scanner so we can use it!
 import java.util.Scanner;
 
-public class Main {
+public class Debug {
     public static void main(String[] args) {
         //I've already set up a scanner with the name 'sc' for you!
         Scanner sc = new Scanner(System.in);
-
         //here's a good working example for you to reference!
         //first: we ask our user for our prompt
         System.out.println("What is your name? ");
@@ -13,22 +12,26 @@ public class Main {
         String name = sc.nextLine();
         //third: we print it back out!
         System.out.println("Hello, " + name + "! ");
-
-
-
+        
 
         //finish this one for me by receiving and printing the user's age back to them
         System.out.println("How old are you? ");
+        //I am going to return the age as done previously with the name using the next function
+        Integer age = sc.nextInt();
+        //I am going to display the users name and age in a sentence.
+        System.out.println("Hello, " + name + " you are " + age + " years old!");
 
 
         //can you do some math for me? take these variable and print out the sum of them!
         int num1 = 10;
         int num2 = 37;
         //?
+        Integer sum = num1 + num2;
 
-        
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum );
+
+
+    
 
     }
 }
-
-
---
+++ Homework 1/test2.java
@@ -0,0 +1,20 @@
+public class test2 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("hello world");
+        System.out.println(12);
+
+
+        String Myname = "Taj";
+        System.out.println(Myname);
+        Integer Myage  = 18;
+
+        System.out.println("My name is " + Myname + " and I am "+ Myage + " years old ");
+
+
+
+
+}
+
+}
