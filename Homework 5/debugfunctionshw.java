55d010d42f3d7aab524f9e3f4f7a03485bde40db
taj106 <sirtaj.pandher1@marist.edu>
2024-11-01 00:54:21 -0400

    Update debugfunctionshw.java

--- Homework 5/debugfunctionshw.java
+++ Homework 5/debugfunctionshw.java
@@ -1,3 +1,15 @@
+/*
+Referencing your homework from https://github.com/melissachodziutko/CMPT220/blob/main/Homework%203/LoopsDebug.java
+Can you take problems 2, 3, and 5 and turn them into methods, and then call those methods in main?
+Things that may trip you up while working on this:
+1. the code works the same whether it's in a method or not, it just needs some minor variable adjustments probably
+2. be wary of opening and closing brackets!! 
+3. methods can't go inside methods!
+4. methods go outside public static main, but inside public class main!
+5. do you need to return the value? if so, make sure it isn't void
+
+I give you two fully working method examples with and without parameters and called them for your reference :)
+*/
 import java.util.Scanner;
 
 public class Main {
